939852816345a02e11a6c96260d91a3a7c30dda3
EladB1 <eb86@njit.edu>
2022-07-06 16:13:03 -0400

    Got basic version of login app working.

---
+++ src/main/java/com/piedpiper/authdemo/controller/HomeController.java
@@ -0,0 +1,28 @@
+package com.piedpiper.authdemo.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+    private Map<String, LocalDateTime> usersLastAccess = new HashMap<>();
+
+    @GetMapping("/")
+    public String getCurrentUser(@AuthenticationPrincipal User user, Model model) {
+        String username = user.getUsername();
+
+        model.addAttribute("username", username);
+        model.addAttribute("lastAccess", usersLastAccess.get(username));
+
+        usersLastAccess.put(username, LocalDateTime.now());
+
+        return "home";
+    }
+}
