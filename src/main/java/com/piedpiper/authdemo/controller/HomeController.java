72f28535cdfbd69cb2c4bc2ac663c0da21c463e1
Elad Bergrin <EladB1@users.noreply.github.com>
2022-07-31 12:49:47 -0400

    Fixed issue with AuthenticationPrincipal caused by switch from basicAuth to JWT.

--- src/main/java/com/piedpiper/authdemo/controller/HomeController.java
+++ src/main/java/com/piedpiper/authdemo/controller/HomeController.java
@@ -1,10 +1,13 @@
 package com.piedpiper.authdemo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.piedpiper.authdemo.user.UserService;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -12,11 +15,15 @@ import java.util.Map;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    UserService userService;
+
     private Map<String, LocalDateTime> usersLastAccess = new HashMap<>();
 
     @GetMapping("/")
-    public String getCurrentUser(@AuthenticationPrincipal User user, Model model) {
-        String username = "TEst"; //user.getUsername();
+    public String getCurrentUser(@AuthenticationPrincipal(expression = "@userService.loadUserByUsername(#this)") User user, Model model) {
+        String username = user.getUsername();
 
         model.addAttribute("username", username);
         model.addAttribute("lastAccess", usersLastAccess.get(username));
