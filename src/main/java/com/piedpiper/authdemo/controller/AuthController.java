4ab663bf33c53b94fc7b325a065b4eacbf50f152
EladB1 <eb86@njit.edu>
2022-08-09 13:51:15 -0400

    Fixed cookie logic and added small amount of logging.

--- src/main/java/com/piedpiper/authdemo/controller/AuthController.java
+++ src/main/java/com/piedpiper/authdemo/controller/AuthController.java
@@ -6,9 +6,11 @@ import com.piedpiper.authdemo.JWT.JWTResponseDTO;
 import com.piedpiper.authdemo.JWT.JWTUtil;
 import com.piedpiper.authdemo.user.AppUser;
 import com.piedpiper.authdemo.user.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -25,6 +27,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -41,6 +44,8 @@ public class AuthController {
 
     @Value("${jwt.maxage.seconds}")
     private int sessionExpiration;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public AuthController(JWTUtil jwtUtil, PasswordEncoder passwordEncoder, UserService userDetailsService, JWTBlockListService blockListService) {
@@ -68,35 +73,46 @@ public class AuthController {
     }
 
     @PostMapping("/token")
-    public ResponseEntity<JWTResponseDTO> token(@RequestBody AppUser appUser, HttpServletResponse response) {
+    public ResponseEntity<JWTResponseDTO> token(@RequestBody AppUser appUser, HttpServletResponse response, HttpServletRequest request) {
         try {
             UserDetails user = userDetailsService.loadUserByUsername(appUser.getUsername());
             if (!passwordEncoder.matches(appUser.getPassword(), user.getPassword())) {
                 throw new BadCredentialsException("Invalid username or password");
             }
             String token = jwtUtil.generateToken(appUser.getUsername());
-            JWTResponseDTO jwtresponse = new JWTResponseDTO(null, token);
+            JWTResponseDTO jwtResponse = new JWTResponseDTO(null, token);
             Cookie cookie = new Cookie("Token", token);
             cookie.setMaxAge(sessionExpiration);
             cookie.setHttpOnly(true);
             cookie.setSecure(false);
             response.addCookie(cookie);
-            return ResponseEntity.ok().body(jwtresponse);
+            logger.info("Successful login for user '" + appUser.getUsername() + "' from IP address: " + request.getRemoteAddr());
+            return ResponseEntity.ok().body(jwtResponse);
         }
         catch(UsernameNotFoundException err) {
-            JWTResponseDTO jwtresponse = new JWTResponseDTO("Invalid username or password", null);
-            return new ResponseEntity<>(jwtresponse, HttpStatus.BAD_REQUEST);
+            JWTResponseDTO jwtResponse = new JWTResponseDTO("Invalid username or password", null);
+            logger.info("Failed login for non-existing user '" + appUser.getUsername() + "' from IP address: " + request.getRemoteAddr());
+            return new ResponseEntity<>(jwtResponse, HttpStatus.BAD_REQUEST);
         }
         catch (AuthenticationException err) {
-            JWTResponseDTO jwtresponse = new JWTResponseDTO(err.getMessage(), null);
-            return new ResponseEntity<>(jwtresponse, HttpStatus.BAD_REQUEST);
+            JWTResponseDTO jwtResponse = new JWTResponseDTO(err.getMessage(), null);
+            logger.info("Failed login for user '" + appUser.getUsername() + "' from IP address: " + request.getRemoteAddr());
+            return new ResponseEntity<>(jwtResponse, HttpStatus.BAD_REQUEST);
         }
     }
 
     @PostMapping("/logout")
-    public ResponseEntity<String> logout(@CookieValue("Token") String token) {
+    public ResponseEntity<String> logout(@CookieValue("Token") String token, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         blockListService.save(new JWTBlockList(token));
+        Cookie cookie = new Cookie("Token", null);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(false);
+        cookie.setMaxAge(0);
+        cookie.setDomain("localhost");
+        cookie.setPath("/");
+        // NOTE: setDomain() and setPath() matching the cookie must match in order to override it
+        response.addCookie(cookie);
         auth.setAuthenticated(false);
         SecurityContextHolder.clearContext();
         return ResponseEntity.ok().body("");
